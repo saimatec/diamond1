9292a97ae950d209d9bf3b873b096f0c3a2bfa68
kunal <kunal_waghray@yahoo.co.in>
2015-03-14 23:27:36 +0530

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def gallery
+  end
+  def contactus
+  end
+  def new
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diamond1::Application.config.secret_token = '31ab13a119a9d9c890de7aeefa4c15ef7dd55f917f533bdf1717a32e26e16a0f7968ac98ecdc6f571a4123fbdc22faa744fedb7a39d93bd4baf6c9df85c085ca'
